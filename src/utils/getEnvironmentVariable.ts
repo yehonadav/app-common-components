b5dc79d4ef64fc53e12fc8a761982894665a73b8
yonadav <yonadav.barilan@gmail.com>
2021-10-30 16:52:29 +0300

    update

--- src/utils/getEnvironmentVariable.ts
+++ src/utils/getEnvironmentVariable.ts
@@ -1,9 +1,9 @@
-export const getEnvironmentVariable = (name:string):string => {
+export const getEnvironmentVariable = <T=string>(name:string):T => {
   if (!process.env[name])
     throw `missing required environment variable process.env.${name}`;
 
-  return process.env[name]!;
+  return process.env[name]! as T;
 }
 
-export const getEnv = (key: string, required: boolean = false): string =>
-    required ? getEnvironmentVariable(key) : process.env[key]!;
+export const getEnv = <T=string>(key: string, required: boolean = false): T =>
+    required ? getEnvironmentVariable(key) : process.env[key]! as T;
